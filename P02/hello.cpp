778acbf04bb41daa367fb73952346eebafdee33f
Rui Melo <44201826+rufimelo99@users.noreply.github.com>
2019-11-18 17:44:14 +0000

    Add files via upload

---
+++ P02/hello.cpp
@@ -0,0 +1,17 @@
+/*
+** Tomás Oliveira e Silva, AED, September 2014
+**
+** my first C++ program (compare with hello.c)
+*/
+
+#include <iostream>
+
+int main(void)
+{
+	for(int i = 1; i< 11; i++)
+	{
+		std::cout << i << "\n";
+  
+		}
+  // return 0;  NOTE that in C++ main() returns 0 if no return value is given
+}
